f530261304f24135de73f3028624214faca682c3
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2023-08-03 18:55:01 -0300

    cargando datos al 1er obj

--- src/main/java/com/ap/homebanking/HomebankingApplication.java
+++ src/main/java/com/ap/homebanking/HomebankingApplication.java
@@ -1,7 +1,11 @@
 package com.ap.homebanking;
 
+import com.ap.homebanking.models.Client;
+import com.ap.homebanking.repositories.ClientRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HomebankingApplication {
@@ -9,5 +13,16 @@ public class HomebankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HomebankingApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner initData( ClientRepository clientRepository) {
+		return (args) -> {
+
+			clientRepository.save(new Client("Jack", "Bauer"));
+
+		};
+	}
+
+
 
 }
