46181f07479f3fa7b7b04005f1040ef48f73451f
Leandro Javier Martinez <leamartinez@santandertecnologia.com.ar>
2023-08-31 13:01:19 -0300

    cargando datos al 1er obj

--- src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
+++ src/main/java/com/ap/homebanking/configurations/WebAuthentication.java
@@ -38,3 +38,19 @@ public class WebAuthentication extends GlobalAuthenticationConfigurerAdapter {
     }
 
 }
+// En task 6:
+// WebAuthentication Falta encapsular la propiedad. Falta crear el rol de Admin. Los permitAll() por lo general se dejan arriba de todo
+/*
+Correccion task5 : Ok Leandro!! Fijate que fienes codigo sepaguetti en tus clases, hay cosas mezcladas por ejemplo en tu clase Cient tienes
+ propiedades entre tus guetters y setters al igual que los metodos propios de la clase no deberian estar entre tus guetters y setters.
+  En account tienes el mismo problema  Fijate que en clientLoan lo has acomodado sabiendo que era ya para ti mas facil de leer
+  En card ya de nuevo el client que tienes dentro de la clase card es una propiedad de card y deberia de estar con el resto de tus propiedades
+   Pero bueno son cositas que no afectan en funcionamiento pero seran de gran aporte para ti y tu equipo cuando tengan que  buscar algo en
+   especifico o hacerle mantenimiento a tu codigo.. te dejo la tarea aprobada!!
+ */
+/*
+task 7
+Calificación: Hay que validar que los números random de la cuenta y tarjeta no existan en la base de datos antes de instanciar los objetos.
+ Hay que validar lo que llega por parámetro no este vacío como el color. El mensaje que hay que retornar no hay que ponerlo en un println,
+ hay que mandarlo con el ResponseEntity. Hay que crear un rol Admin.
+ */
